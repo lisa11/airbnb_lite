a4cde5aea7cdcbaacf97c05477e2aed339cd3f2e
Raghu Betina <raghu@firstdraft.com>
2017-01-11 14:31:57 +0000

    Direct association between review_of_rooms and bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,8 @@
 class Booking < ApplicationRecord
   # Direct associations
 
+  belongs_to :review_of_room
+
   belongs_to :review_of_guest
 
   belongs_to :request
--- app/models/review_of_room.rb
+++ app/models/review_of_room.rb
@@ -1,6 +1,9 @@
 class ReviewOfRoom < ApplicationRecord
   # Direct associations
 
+  has_one    :booking,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
